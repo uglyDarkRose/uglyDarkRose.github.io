178300411a6bb46d606c82d58d2620ad3bb86a65
uglyDarkRose <108894654+uglyDarkRose@users.noreply.github.com>
2022-10-15 16:51:53 +0300

    Add files via upload

---
+++ App/Scripts/js-script.js
@@ -0,0 +1,9 @@
+var h3Showed = false;
+
+function changeDisplay() {
+	const header3 = document.getElementById("1");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed
+}
+
+
